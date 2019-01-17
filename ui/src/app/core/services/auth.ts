c4b3015e833684eaebe2a1dde569551fa5562f9b
Asen Prodanov <Asen.Prodanov@Softwaregroup-bg.com>
2019-01-17 02:01:55 +0200

    add add/get/purge functions for oauth token

--- ui/src/app/core/services/auth.ts
+++ ui/src/app/core/services/auth.ts
@@ -1,16 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { server } from '../../../environments/server';
+import { gitHubAPIs } from '../../../environments/server';
+const tokenName = 'token';
 
 @Injectable()
 export class AuthService {
-	constructor(private http: HttpClient) {}
 
-	login(username: string, password: string): Observable<any> {
-		return this.http.post(
-			`${server}/login`,
-			{ username: username, password: password }
-		);
+	constructor(private http: HttpClient) {
+	}
+
+	login(): Observable<any> {
+		return this.http.get(gitHubAPIs.authorize);
+	}
+
+	getToken(): String {
+		return window.localStorage[tokenName];
+	}
+
+	saveToken(token: String) {
+		window.localStorage[tokenName] = token;
+	}
+
+	destroyToken() {
+		window.localStorage.removeItem(tokenName);
 	}
 }
