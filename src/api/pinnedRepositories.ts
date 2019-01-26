bd0b093cb9e559d511f05eb21ff6fe50b0f57a9f
Asen Prodanov <Asen.Prodanov@Softwaregroup-bg.com>
2019-01-26 17:45:48 +0200

    promisify request package in pinnedRepositories route with bluebird

--- src/api/pinnedRepositories.ts
+++ src/api/pinnedRepositories.ts
@@ -1,49 +1,42 @@
-import request from 'request';
+import bluebird from 'bluebird';
+import _request from 'request';
 import { getHeaders } from './utils/helpers';
 import { rest, graphQl } from './utils/constants';
 import { Express } from 'express';
+const request: any = bluebird.promisifyAll(_request);
 
 export = (app: Express) => {
 	app.post('/pinnedRepositories', async (req: any, res: any) => {
-
 		const organization = req.body.organization;
 		const token = req.body.token;
-		let result: Array<any> = [];
 
-		await new Promise(resolve => request.post({
+		const repositories = JSON.parse((await request.postAsync({
 			url: graphQl,
 			headers: getHeaders(token),
 			body: JSON.stringify({
 				query: pinnedRepositoriesQuery(organization)
 			})
-		}, (error: any, response: any, body: any) => {
-			result = JSON.parse(body).data.repositoryOwner.pinnedRepositories.edges.map((item: any) => {
-				return {
-					name: item.node.name,
-					license: item.node.licenseInfo.name,
-					collaborators: item.node.mentionableUsers.totalCount,
-					releases: item.node.releases.totalCount,
-					commits: item.node.defaultBranchRef.target.history.totalCount
-				};
-			});
-			resolve();
-		}));
-
-		const temp: Array<any> = [];
-
-		result.forEach((item: any, index: number) => {
-			temp.push(new Promise(resolve => {
-				request.get({
-					uri: `${rest}/repos/${organization}/${item.name}/branches`,
-					headers: getHeaders(token)
-				}, (_error, _response, _body) => {
-					result[index].branches = JSON.parse(_body);
-					resolve();
-				});
-			}));
+		})).body).data.repositoryOwner.pinnedRepositories.edges.map((item: any) => {
+			return {
+				name: item.node.name,
+				license: item.node.licenseInfo.name,
+				collaborators: item.node.mentionableUsers.totalCount,
+				releases: item.node.releases.totalCount,
+				commits: item.node.defaultBranchRef.target.history.totalCount
+			};
 		});
 
-		await Promise.all(temp);
+		const result = await Promise.all(repositories.map(async (item: any) => {
+			const branches = JSON.parse((await request.getAsync({
+				uri: `${rest}/repos/${organization}/${item.name}/branches`,
+				headers: getHeaders(token)
+			})).body);
+
+			item.branches = branches;
+
+			return item;
+		}));
+
 		res.send(result);
 	});
 };
